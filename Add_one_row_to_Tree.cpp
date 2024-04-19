336a4e06cb37d96cdd6fd10dfc8b210f9368e557
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-04-19 22:28:47 +0530

    Simple logic ,can also be solved using level order traversal

---
+++ Add_one_row_to_Tree.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* solve(TreeNode*root,int val,int depth,int curr){
+        if(!root){return nullptr;}
+        if(curr == depth -1){
+            TreeNode*l = root->left;
+            TreeNode*r = root->right;
+            root->left = new TreeNode(val);
+            root->right = new TreeNode(val);
+            root->left->left = l;
+            root->right->right = r;
+        }
+        solve(root->left,val,depth,curr+1);
+        solve(root->right,val,depth,curr+1);
+        return root;
+    }
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth ==1){
+            TreeNode*newRoot = new TreeNode(val);
+            newRoot->left = root;
+            return newRoot;
+        }
+        return solve(root,val,depth,1);
+    }
+};
