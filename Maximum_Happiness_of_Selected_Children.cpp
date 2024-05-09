54e9643f5ce33702b85ac12707feb03e77a79654
alpha-asb <153850077+alpha-asb@users.noreply.github.com>
2024-05-09 08:05:21 +0530

    sort ; (arr[i] - i >=0 ) ?ans+= arr[i] - i : return ans;

---
+++ Maximum_Happiness_of_Selected_Children.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long maximumHappinessSum(vector<int>& happiness, int k) {
+        sort(happiness.rbegin(),happiness.rend());
+        long long ans=0;
+        for(int i=0;i<k;i++){
+          if(happiness[i]-i <=0){return ans;}
+          else{
+            ans += happiness[i]-i;
+          }
+        }
+        return ans;
+    }
+};
